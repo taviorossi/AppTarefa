6324d5bd4799a1c408869ee69a81d85e8250b289
AzureAD\OctavioJoséRodrigues <octavio.rossi@gamasti.com.br>
2022-05-27 11:09:09 -0300

    AppTarefa

---
+++ AppTarefa/AppTarefa/Telas/Listar.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTarefa.Telas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listar : ContentPage
+    {
+        public Listar()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCadastrar(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Cadastrar());
+        }
+
+        private void BtnVisualizar(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Telas.Visualizar());
+        }
+    }
+}
---
+++ AppTarefa/AppTarefa/Telas/Visualizar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTarefa.Telas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Visualizar : ContentPage
+    {
+        public Visualizar()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVoltar(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
